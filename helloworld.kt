7cf3f287f03a3cb42efbde59a75d96fa84f7eb9b
LiamBartolini <liambartolini@gmail.com>
2021-04-30 22:57:52 +0200

    mutable lists

--- helloworld.kt
+++ helloworld.kt
@@ -1,8 +1,11 @@
 fun sayHello(greeting:String, itemToGreet:String) = println("$greeting $itemToGreet")
 
 fun main() {
-    val interestingThings = listOf("Kotlin", "Programming", "Comic books")
+    val interestingThings = mutableListOf("Kotlin", "Programming", "Comic books")
+    interestingThings.add("Dogs")
+    interestingThings.forEach { println(it) }
 
-    val map = mapOf(1 to "a", 2 to "b", 3 to "c")
+    val map = mutableMapOf(1 to "a", 2 to "b", 3 to "c")
+    map.put(4, "d")
     map.forEach { key, value -> println("$key -> $value") }
 }
