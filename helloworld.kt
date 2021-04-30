14ed320cca4b84a8473cab02ed7c42f5a0c5dce2
LiamBartolini <liambartolini@gmail.com>
2021-04-30 22:43:34 +0200

    for loop

--- helloworld.kt
+++ helloworld.kt
@@ -3,6 +3,8 @@ fun sayHello(greeting:String, itemToGreet:String) = println("$greeting $itemToGr
 fun main() {
     val interestingThings = arrayOf("Kotlin", "Programming", "Comic books")
     println(interestingThings.size)
-    println(interestingThings[0])
-    println(interestingThings.get(0))
+
+    for (interestingThing in interestingThings) {
+        println(interestingThing)
+    }
 }
